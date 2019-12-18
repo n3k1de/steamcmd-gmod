cfa4206c036aa062d1c22153940318c9dab44789
DJustDE <mail@djust.de>
2019-12-18 21:21:09 +0100

    Update entrypoint.sh

--- data/entrypoint.sh
+++ data/entrypoint.sh
@@ -3,6 +3,6 @@
 cd ${STEAMCMDDIR}
 
 # wget -O start.sh https://play.djust.de/steamcmd/request/ttt/${HOSTNAME}/start.sh
-wget -O start.sh https://raw.githubusercontent.com/djust-de/steamcmd/master/bash/start-gmod.sh
+wget -O start.sh https://raw.githubusercontent.com/djust-de/gmod/master/bash/start-gmod.sh
 chmod -R 0755 ./start.sh
 ./start.sh
