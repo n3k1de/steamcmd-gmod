ef983f9cffa04ea03ef83ff60f1d41ebad7b5004
DJustDE <mail@djust.de>
2019-12-22 18:57:08 +0100

    Update entrypoint.sh

--- data/entrypoint.sh
+++ data/entrypoint.sh
@@ -5,4 +5,4 @@ cd ${STEAMCMDDIR}
 # wget -O start.sh https://play.djust.de/steamcmd/request/ttt/${HOSTNAME}/start.sh
 wget -O start.sh https://raw.githubusercontent.com/djust-de/gmod/master/bash/start-gmod.sh 
 chmod -R 0775 ./start.sh && chown -R steam ./start.sh ${STEAMCMDDIR} ${SERVERDIR}
-su steam -c "./start.sh"
+su steam -c "ln -s ${STEAMCMDDIR}/linux32/steamclient.so ~/.steam/sdk32/steamclient.so && ./start.sh"
