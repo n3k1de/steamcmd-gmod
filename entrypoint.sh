36dfe3912b7c2d4931078cd42256d6b2bead0c48
DJustDE <44903208+djust-de@users.noreply.github.com>
2020-01-01 23:35:26 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ulimit -n 2048‬
-IP="$(curl -4 https://djust.de/dns/ip)"
+IP="$(curl -s4 http://v4.ipv6-test.com/api/myip.php)"
 cd ${STEAMCMDDIR}
 
 if [ -e "/home/steam/.steam/sdk32/steamclient.so" ]
@@ -16,7 +16,6 @@ else
   fi
 fi
 
-rm ./start.sh
-wget -O start.sh https://raw.githubusercontent.com/djust-de/steamcmd/master/bash/garrysmod-start.sh
+rm ./start.sh && curl -so start.sh https://raw.githubusercontent.com/djust-de/steamcmd/master/bash/garrysmod-start.sh
 chmod -R 0775 ./start.sh && chown -R steam ./start.sh ${STEAMCMDDIR} ${SERVERDIR}
 su steam -c "./start.sh"
