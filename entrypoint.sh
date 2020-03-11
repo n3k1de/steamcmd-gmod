e908a576e5b96f2bb41b4b2a151d4d233fd138b8
xWilliam <44903208+xWilli4m@users.noreply.github.com>
2020-03-11 19:25:52 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -71,7 +71,7 @@ su steam -c "cd ${SERVERDIR}/gmod/
   -console -nobreakpad -usercon -secure -debug \
   -authkey ${APIKEY} \
   -port ${PORT} \
-  -ip "localhost" \
+  -ip "127.0.0.1" \
   +port ${PORT} \
   +clientport ${CLIENTPORT} \
   +maxplayers ${MAXPLAYERS} \
