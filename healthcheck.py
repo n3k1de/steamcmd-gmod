c2d11f18c5683fef5b06323cefb86183e4db9f9c
xWilliam <44903208+xWilli4m@users.noreply.github.com>
2020-03-14 23:18:11 +0100

    Update healthcheck.py

--- healthcheck.py
+++ healthcheck.py
@@ -3,10 +3,9 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.settimeout(1.0)
 s.connect((socket.gethostbyname(socket.gethostname()), int(sys.argv[1])))
 try:
-	s.send(b'\xFF\xFF\xFF\xFF\x550xFFFFFFFF')
+	s.send(b'\xFF\xFF\xFF\xFFTSource Engine Query\x00')
 	s.recv(4096)
 	print(0)
-	exit(0)
 except socket.error as e:
 	print(1)
-	exit(1)
+	raise SystemExit(1)
