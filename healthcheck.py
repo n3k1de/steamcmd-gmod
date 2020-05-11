0d7edc2ba3a33dcfdb1476d0a781e867c75748e3
xWilliam <44903208+xWilli4m@users.noreply.github.com>
2020-05-11 19:38:05 +0200

    Update healthcheck.py

--- healthcheck.py
+++ healthcheck.py
@@ -1,11 +1,13 @@
-import socket, sys
+import socket, sys, time
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.settimeout(9.0)
 s.connect((socket.gethostbyname(socket.gethostname()), int(sys.argv[1])))
 try:
 	s.send(b'\xFF\xFF\xFF\xFFTSource Engine Query\x00')
 	s.recv(4096)
-	print(0)
+	
 except socket.error as e:
-	print(1)
+	with open("/opt/server/socket.txt", "a+") as ffile:
+		ffile.write(time.strftime('%Y-%m-%d %H:%M:%S')+':\t'+e)
+		ffile.write('\n')
 	raise SystemExit(1)
