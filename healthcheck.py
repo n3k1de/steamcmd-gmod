b3a6d826a1da78e91f59d9aa5fd14051aeaa6ef4
xWilliam <44903208+xWilli4m@users.noreply.github.com>
2020-05-11 20:25:43 +0200

    Update healthcheck.py

--- healthcheck.py
+++ healthcheck.py
@@ -6,10 +6,10 @@ try:
 	s.send(b'\xFF\xFF\xFF\xFFTSource Engine Query\x00')
 	r = s.recv(4096)
 	with open("/opt/server/socket.log", "a+") as ffile:
-		ffile.write(str(time.strftime('%Y-%m-%d %H:%M:%S'))+':\t'+str(r))
+		ffile.write(str(time.strftime('%Y-%m-%d %H:%M:%S'))+'\t'+str(r))
 		ffile.write('\n')
 except socket.error as e:
 	with open("/opt/server/socket.log", "a+") as ffile:
-		ffile.write(str(time.strftime('%Y-%m-%d %H:%M:%S'))+':\t'+str(e))
+		ffile.write(str(time.strftime('%Y-%m-%d %H:%M:%S'))+'\t'+str(e))
 		ffile.write('\n')
 	raise SystemExit(1)
