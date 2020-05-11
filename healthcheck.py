810ca486712d225fb08006d22c78ab125c2f3f39
xWilliam <44903208+xWilli4m@users.noreply.github.com>
2020-05-11 20:25:59 +0200

    Update healthcheck.py

--- healthcheck.py
+++ healthcheck.py
@@ -1,6 +1,6 @@
 import socket, sys, time
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.settimeout(9.0)
+s.settimeout(12.0)
 s.connect((socket.gethostbyname(socket.gethostname()), int(sys.argv[1])))
 try:
 	s.send(b'\xFF\xFF\xFF\xFFTSource Engine Query\x00')
